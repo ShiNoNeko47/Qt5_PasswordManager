876948d84a6a12e8b3adca1b17aadbd3053b3241
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-04-25 14:59:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,39 +23,83 @@ class MainWindow(QWidget):
         self.key_input.textChanged.connect(self.check_key)
         self.layout.addWidget(self.key_input)
 
-        self.addPassword_btn = QPushButton('Add password')
-        self.addPassword_btn.setEnabled(False)
-        self.addPassword_btn.clicked.connect(self.newpassword)
-        self.layout.addWidget(self.addPassword_btn)
+        self.displayPasswords_btn = QPushButton('Display passwords')
+        self.displayPasswords_btn.setEnabled(False)
+        self.displayPasswords_btn.clicked.connect(self.displaypasswords)
+        self.layout.addWidget(self.displayPasswords_btn)
 
-        self.showPasswords_btn = QPushButton('Show my passwords')
-        self.showPasswords_btn.setEnabled(False)
-        self.showPasswords_btn.clicked.connect(self.showpasswords)
-        self.layout.addWidget(self.showPasswords_btn)
+        self.managePasswords_btn = QPushButton('Manage passwords')
+        self.managePasswords_btn.setEnabled(False)
+        self.managePasswords_btn.clicked.connect(self.managepasswords)
+        self.layout.addWidget(self.managePasswords_btn)
 
         self.setLayout(self.layout)
 
     def check_key(self):
-        self.addPassword_btn.setEnabled(False)
-        self.showPasswords_btn.setEnabled(False)
+        self.managePasswords_btn.setEnabled(False)
+        self.displayPasswords_btn.setEnabled(False)
         if self.key_input.text() == '123':
-            self.addPassword_btn.setEnabled(True)
-            self.showPasswords_btn.setEnabled(True)
+            self.managePasswords_btn.setEnabled(True)
+            self.displayPasswords_btn.setEnabled(True)
 
-    def newpassword(self):
+    def managepasswords(self):
         if self.w1 == None:
-            self.w1 = NewPasswordWindow()
+            self.w1 = ManagePasswordsWindow()
         self.w1.show()
 
-    def showpasswords(self):
+    def displaypasswords(self):
         if self.w2 == None:
             self.w2 = ShowPasswords()
         self.w2.show()
 
-class NewPasswordWindow(QWidget):
+class ManagePasswordsWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('New Password')
+        self.setWindowTitle('Manage Passwords')
+        self.layout = QGridLayout()
+
+        self.newWebsite_le = QLineEdit()
+        self.layout.addWidget(self.newWebsite_le, 0, 0)
+
+        self.newUsername_le = QLineEdit()
+        self.layout.addWidget(self.newUsername_le, 0, 1)
+
+        self.newPassword_le = QLineEdit()
+        self.newPassword_le.setEchoMode(QLineEdit.Password)
+        self.layout.addWidget(self.newPassword_le, 0, 2)
+
+        self.add_btn = QPushButton('Add')
+        self.layout.addWidget(self.add_btn, 0, 3)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.verticalHeader().setVisible(False)
+        self.table.setHorizontalHeaderLabels(['Website', 'Username', 'Password', ''])
+
+        conn = sqlite3.connect('passwords.db')
+        c = conn.cursor()
+        c.execute('select * from passwords')
+        self.data = c.fetchall()
+        c.close()
+        conn.close()
+
+        for i in range(3):
+            self.table.setColumnWidth(i, 190)
+        self.table.setColumnWidth(3, 30)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        self.remove_btns = []
+        for row, i in zip(self.data, range(len(self.data))):
+            self.table.insertRow(i)
+            for data, j in zip(row, range(3)):
+                self.table.setItem(self.data.index(row), j, (QTableWidgetItem(data) if j != 2 else QTableWidgetItem('*'*len(data))))
+
+            self.remove_btns.append(remove_btn(self.table, self.remove_btns))
+            self.table.setCellWidget(i, 3, self.remove_btns[i])
+
+        self.layout.addWidget(self.table, 1, 0, 1, 4)
+
+        self.setLayout(self.layout)
 
 class ShowPasswords(QWidget):
     def __init__(self):
@@ -99,6 +143,17 @@ class copy_btn(QPushButton):
     def __init__(self, index, data):
         super().__init__()
         self.clicked.connect(lambda: pyperclip.copy(data[index][2]))
+
+class remove_btn(QPushButton):
+    def __init__(self, table, remove_btns):
+        super().__init__()
+        self.table = table
+        self.remove_btns = remove_btns
+        self.clicked.connect(self.remove_row)
+
+    def remove_row(self):
+        self.table.removeRow(self.remove_btns.index(self))
+        del self.remove_btns[self.remove_btns.index(self)]
 
 def main():
 
