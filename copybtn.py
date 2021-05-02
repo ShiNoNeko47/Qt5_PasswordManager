ee2128884f2c78328c413b41ad36f970980ea5b6
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-05-02 16:59:48 +0200

    Update copybtn.py

--- copybtn.py
+++ copybtn.py
@@ -5,5 +5,6 @@ class Copy_btn(QPushButton):
     def __init__(self, index, data, f):
         super().__init__()
         self.clicked.connect(lambda: pyperclip.copy(f.decrypt(data[index][2].encode()).decode()))
+        self.setText('📋')
 
 
