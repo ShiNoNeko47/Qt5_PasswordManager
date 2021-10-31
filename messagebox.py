52a733b336b9cb1300eeef50eca1574e3a175167
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-10-31 20:51:45 +0100

    Update messagebox.py
    made messagebox universal

--- messagebox.py
+++ messagebox.py
@@ -2,28 +2,39 @@ from PyQt5.QtWidgets import *
 from removebtn import Remove_btn
 
 class MessageBox(QWidget):
-    def __init__(self, managePasswordsWindow):
+    def __init__(self, managePasswordsWindow, mode = 'yes/no', title = 'Save changes?'):
         super().__init__()
         self.managePasswordsWindow = managePasswordsWindow
 
-        self.setWindowTitle('Save changes?')
+        self.setWindowTitle(title)
         self.layout = QGridLayout()
 
         self.label = QLabel()
-        self.label.setText('Save changes?')
+        self.label.setText(title)
         self.layout.addWidget(self.label, 0, 0, 1, 2)
+
+        self.ok_btn = QPushButton()
+        self.ok_btn.setText('Ok')
+        self.ok_btn.clicked.connect(self.ok)
 
         self.yes_btn = QPushButton()
         self.yes_btn.setText('Yes')
         self.yes_btn.clicked.connect(self.save)
-        self.layout.addWidget(self.yes_btn, 1, 1)
 
         self.no_btn = QPushButton()
         self.no_btn.setText('No')
         self.no_btn.clicked.connect(self.not_save)
-        self.layout.addWidget(self.no_btn, 1, 0)
+
+        if mode == 'yes/no':
+            self.layout.addWidget(self.yes_btn, 1, 1)
+            self.layout.addWidget(self.no_btn, 1, 0)
+        elif mode == 'ok':
+            self.layout.addWidget(self.ok_btn, 1, 0)
 
         self.setLayout(self.layout)
+
+    def ok(self):
+        self.close()
 
     def save(self):
         self.managePasswordsWindow.commitChanges()
