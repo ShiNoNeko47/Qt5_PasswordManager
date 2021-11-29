a2a0cb06ceddeddaf8f306a2dcf23238f52df78e
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-11-29 20:51:01 +0100

    Update removebtn.py

--- qpassword_manager/btns/removebtn.py
+++ qpassword_manager/btns/removebtn.py
@@ -4,14 +4,14 @@ from PyQt5.QtWidgets import QPushButton
 class Remove_btn(QPushButton):
     marked = []
 
-    def __init__(self, rowId, table, remove_btns, sql, window):
+    def __init__(self, rowId, table, remove_btns, actions, window):
         super().__init__()
         # print(Remove_btn.marked)
         self.table = table
         self.remove_btns = remove_btns
         self.clicked.connect(self.remove_row)
         self.rowId = rowId
-        self.sql = sql
+        self.actions = actions
         self.w = window
         if self.rowId not in Remove_btn.marked:
             self.setText('X')
@@ -20,17 +20,13 @@ class Remove_btn(QPushButton):
 
     def remove_row(self):
         if self.rowId not in Remove_btn.marked:
-            self.statement = """update Passwords
-                                set
-                                Deleted = 1
-                                where
-                                (ID = {})""".format(self.rowId)
-            self.sql.append(self.statement)
+            self.action = [self.rowId,
+                           'delete']
+            self.actions.append(self.action)
             self.setText('-')
             Remove_btn.marked.append(self.rowId)
         else:
-            del self.sql[self.sql.index(self.statement)]
+            del self.actions[self.actions.index(self.action)]
             self.setText('X')
             del Remove_btn.marked[Remove_btn.marked.index(self.rowId)]
-        self.w.save_btn.setDisabled(not self.sql)
-        # print(self.sql)
+        self.w.save_btn.setDisabled(not self.actions)
