45df9e2cc458d65f6f748b99cb2f46eb90c69a52
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-12-16 14:15:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ if __name__ == "__main__":
         author_email="nikola.brezovec.32123@gmail.com",
         packages=find_packages(),
         include_package_data=True,
+        install_requires=[
+            "cryptography",
+            "pycryptodome",
+            "pyperclip",
+            "PyQt5",
+            "requests",
+        ],
         entry_points={
             "gui_scripts": [
                 "qpassword_manager = qpassword_manager.__main__:main"
