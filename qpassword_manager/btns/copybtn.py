f938d2aec27a90ec3db16627008b0dbd2df5bd0d
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-12-19 14:40:51 +0100

    Adding docstrings

--- qpassword_manager/btns/copybtn.py
+++ qpassword_manager/btns/copybtn.py
@@ -1,10 +1,19 @@
+'''CopyBtn class'''
+
 import pyperclip
 from PyQt5.QtWidgets import QPushButton
 
 
 class CopyBtn(QPushButton):
-    def __init__(self, index, data, f):
+    '''
+    Button that copies a decrypted password to clipboard
+
+    Attributes:
+        decrypted: decrypted password
+    '''
+
+    def __init__(self, index, data, fernet):
         super().__init__()
-        self.decrypted = f.decrypt(data[index]["2"].encode()).decode()
+        self.decrypted = fernet.decrypt(data[index]["2"].encode()).decode()
         self.clicked.connect(lambda: pyperclip.copy(self.decrypted))
         self.setText("📋")
