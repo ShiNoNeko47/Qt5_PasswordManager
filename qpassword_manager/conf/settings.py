0358f33fb53e7dba8aa69618e05a61fbcd638b43
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-12-19 14:41:12 +0100

    Adding docstrings

--- qpassword_manager/conf/settings.py
+++ qpassword_manager/conf/settings.py
@@ -1,9 +1,18 @@
+'''Settings window'''
+
 from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit, QGridLayout
 from PyQt5.Qt import Qt
 from qpassword_manager.conf.connectorconfig import Config
 
 
 class Settings(QWidget):
+    '''
+    Settings window
+
+    Attributes:
+        config: configuration in json format
+    '''
+
     def __init__(self):
         super().__init__()
         self.config = Config.config()
@@ -25,16 +34,22 @@ class Settings(QWidget):
         self.setLayout(self.layout)
 
     def check_ip(self):
+        '''Checks if line edit is empty and enables or disables the button'''
+
         if self.host_le.text() != "":
             self.ok_btn.setEnabled(True)
         else:
             self.ok_btn.setEnabled(False)
 
     def config_update(self):
+        '''Updates configuration using Config.config_update method'''
+
         self.config["host"] = self.host_le.text()
         Config.config_update(self.config)
         self.close()
 
     def keyPressEvent(self, event):  # pylint: disable=invalid-name
+        '''Clicks ok button when you press enter'''
+
         if event.key() == Qt.Key_Return:
             self.ok_btn.click()
