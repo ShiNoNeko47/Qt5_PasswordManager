398ccd6f0afd82c17f7f69829b1e37f25d39a3a4
ShiNoNeko47 <nikola.brezovec.32123@gmail.com>
2021-12-19 23:20:25 +0100

    Update setupwindow.py

--- qpassword_manager/setupwindow.py
+++ qpassword_manager/setupwindow.py
@@ -1,3 +1,5 @@
+"""Setup window"""
+
 import logging
 from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QGridLayout
 from PyQt5.Qt import Qt
@@ -8,10 +10,19 @@ from qpassword_manager.conf.connectorconfig import Config
 
 
 class SetupWindow(QWidget):
+    """
+    Setup window
+
+    Attributes:
+        window_main: main window
+
+    """
+
     def __init__(self, window_main):
         super().__init__()
 
         self.window_main = window_main
+
         self.layout = QGridLayout()
         self.setWindowTitle("New user")
         self.setFixedHeight(150)
@@ -42,10 +53,14 @@ class SetupWindow(QWidget):
         self.messagebox = MessageBox(self, "message")
 
     def keyPressEvent(self, event):  # pylint: disable=invalid-name
+        """Clicks ok when you press enter"""
+
         if event.key() == Qt.Key_Return:
             self.ok_btn.click()
 
     def check_password(self):
+        """Checks if password entered is longer than 3 characters"""
+
         self.ok_btn.setEnabled(False)
         if all(
             [
@@ -56,6 +71,8 @@ class SetupWindow(QWidget):
             self.ok_btn.setEnabled(True)
 
     def add_user(self):
+        """Adds new user to database"""
+
         master_key = SHA256.new(self.key_setup_le.text().encode()).hexdigest()
         msg = requests.post(
             Config.config()["host"],
@@ -79,17 +96,29 @@ class SetupWindow(QWidget):
             self.close()
 
     def messagebox_handler(self, choice):
+        """
+        MessageBox choice handler
+
+        Arguments:
+            choice: 1 or 0 based on the button clicked on MessageBox window
+        """
+
         if choice == 1:
             self.window_main.name_input.setText(self.username_setup_le.text())
             self.window_main.key_input.setText("")
             self.close()
 
     def reset_entries(self):
+        """Sets all entries to \"\""""
+
         self.username_setup_le.setText("")
         self.key_setup_le.setText("")
         self.key_reenter_le.setText("")
 
     def closeEvent(self, event):  # pylint: disable=invalid-name
+        """Closes MessageBox and sets all entries to \"\" when closing
+        SetupWindow"""
+
         if self.messagebox.isVisible():
             self.messagebox.close()
         self.reset_entries()
